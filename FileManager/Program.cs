f986339ae5d01f5a994e578e8fcfcda708a8eeba
Дмитрий <@DESKTOP-BHOC453>
2018-07-06 20:28:58 +0300

    some fix

--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileManager
 {
+    public delegate void OnKey(ConsoleKeyInfo key);
+
     class Program
     {
         static void Main(string[] args)
         {
+            FileManager manager = new FileManager();
+            manager.Explore();
         }
     }
 }
